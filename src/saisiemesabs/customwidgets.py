11b4ee2594f9d45788c508f83909efe857333c7f
Arthur Perrin <arthur.perrin01@protonmail.com>
2025-02-01 15:06:51 +0100

    Pep8 et commentaires

--- src/saisiemesabs/customwidgets.py
+++ src/saisiemesabs/customwidgets.py
@@ -12,8 +12,9 @@ angle_re = re.compile(r"^(?:[0-3]*[0-9]{1,2}|400)(?:\.[0-9]{4,})$")
 mesure_re = re.compile(r"^(?:-*[0-9]+)(?:\.[0-9]{1})$")
 date_re = re.compile(r"^\d{2}\/\d{2}\/\d{2}$")
 
-def date_add_seconds(date, sec):
-    """Additionne une horaire au format hhmmss à un nombre de second
+
+def date_add_seconds(date: str, sec: int) -> str:
+    """Additionne une horaire au format hhmmss à un nombre de seconde
 
     Args:
         date (str): Horaire format hhmmss
@@ -22,31 +23,41 @@ def date_add_seconds(date, sec):
     Returns:
         str: horaire au format hhmmss
     """
+    # Découpe du string d'entrée
     hour = int(date[0:2])
     minute = int(date[2:4])
     second = int(date[4:6])
+    # Ajout des secondes
     newSecond = second + sec
+    # Récupération des minutes supplémentaires
     newMinute = minute + int(newSecond / 60)
+    # Récupération des heures supplémentaires
     newHour = hour + int(newMinute / 60)
+    # Recalcule des secondes/minutes/heures
     newSecond %= 60
     newMinute %= 60
     newHour %= 24
-    return str(newHour).zfill(2) + str(newMinute).zfill(2) + str(newSecond).zfill(2)
+    # Renvoie de l'horaire hhmmss
+    return (str(newHour).zfill(2) +
+            str(newMinute).zfill(2) +
+            str(newSecond).zfill(2))
 
 
 class Logo(QtWidgets.QLabel):
-
-    def __init__(self, path, maxHeight):
+    """ Logo a taille fixe
+    """
+    def __init__(self, path: str, maxHeight: int) -> None:
         """Génération d'un label pour afficher un logo
 
         Args:
-            path (str): chemin du logo
+            path (str): Chemin du logo
             maxHeight (int): Hauteur maximale
         """
         super().__init__()
+        # pourquoi ? Parce que ça marche mieux avec -20...
         self.setFixedHeight(
             maxHeight - 20
-        )  # pourquoi ? bah parce que ça marche mieux avec -20
+        )
         logo = QPixmap(path).scaled(
             self.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation
         )
@@ -55,8 +66,9 @@ class Logo(QtWidgets.QLabel):
 
 
 class SaisieDate(QtWidgets.QLineEdit):
-
-    def __init__(self):
+    """ Champs d'edition de la date
+    """
+    def __init__(self) -> None:
         """QtWidgets.QLineEdit pour la saisie d'une date"""
         super().__init__()
         self.setAlignment(Qt.AlignCenter)
@@ -65,118 +77,146 @@ class SaisieDate(QtWidgets.QLineEdit):
 
 
 class MyLineEdit(QtWidgets.QLineEdit):
-
-    def __init__(self, text):
-        """ QtWidgets.QLineEdit à ma sauce pour l'inscription de l'heure/angle/mesure
+    """ Champs d'édition abstraite
+    """
+    def __init__(self, text: str) -> None:
+        """ QtWidgets.QLineEdit à ma sauce pour l'inscription de
+            l'heure/angle/mesure
 
         Args:
-            text (str): Texte d'indication
+            text (str): Texte initial
         """
-        # Variable indiquant si la valeur a été modifiée par l'homme (pour l'autocomplet)
+        # Variable indiquant si la valeur a été modifiée par l'utilisateur
         self.editedByHand = False
         super().__init__()
+        # Text initial
         self.initext = text
-        self.setAlignment(Qt.AlignCenter)
-        self.mousePressEvent = self.press  # Action en cas de clique de souris
         self.setText(text)
+        self.setAlignment(Qt.AlignCenter)
+        # Action en cas de clique de souris
+        self.mousePressEvent = self.press
         self.setFixedWidth(150)
+        # Lorsque due la valeur est editée
         self.textEdited.connect(
             self.edited
-        )  # lorsque due la valeur est editée
+        )
+        # Action en cas de fin d'édition
         self.editingFinished.connect(self.changed)
         self.regexValidator = re.compile(r"")
 
-    def validatepls(self):
-        """Emet un son lorsqu'il y a une erreur d'input"""
+    def validatepls(self) -> None:
+        """ Emet un son lorsqu'il y a une erreur d'input
+        """
         if not self.hasAcceptableInput():
+            # Ne fonctionne pas sur toutes les plateformes
             QtWidgets.QApplication.beep()
 
-    def press(self, o):
-        """Lors du clique de la souris, efface le text d'indication
+    def press(self, o) -> None:
+        """ Lors du clique de la souris, efface le text d'indication
 
         Args:
-            o (?): ?
+            o (?): Non utilisé
         """
         if self.text() == self.initext:
             self.setText("")
 
-    def edited(self):
-        """Appelé lors d'une edition d'origine humaine"""
+    def edited(self) -> None:
+        """ Appelé lors d'une edition d'origine humaine
+        """
         self.editedByHand = True
-        if self.text() != "-":
+        if self.text() != '-':
             self.validatepls()
 
-    def setText(self, string, force=False):
+    def setText(self, text: str, force=False) -> None:
         """Change le texte s'il n'a pas été modifié par l'homme
 
         Args:
-            string (str): nouveau texte
+            text (str): nouveau texte
             force (bool, optional): force l'edition. Defaults to False.
         """
         if not self.editedByHand or force:
-            super().setText(string)
+            super().setText(text)
 
-    def rewrite(self):
-        """A overrider"""
-        pass
-
-    def changed(self):
-        """Executer si la saisie est modifié, verifie si la saisie est valide. Sinon emet un beep"""
+    def changed(self) -> None:
+        """ Si la saisie est modifié, verifie si la saisie est valide
+            Sinon emet un beep"""
         self.rewrite()
         if not self.isValid():
+            # Ne fonctionne pas sur toute les plateformes
             QtWidgets.QApplication.beep()
 
-    def isStrange(self):
-        """à overrider, determine si une saisie est anormale
+    def rewrite(self) -> None:
+        """ Pour heritage
+        """
+        return
+
+    def isStrange(self) -> None:
+        """ Pour heritage
 
         Returns:
             bool: False
         """
         return False
 
-    def isValid(self, color=True):
-        """Verifie si la saisie est valide à partir des regex
-
-        Args:
-            color (bool, optional): Colorie la case selon sa validation . Defaults to True.
+    def isValid(self, color_notvalid: bool = True) -> bool:
+        """ Verifie si la saisie est valide à partir d'un regex
 
         Returns:
             bool: Saisie valide ou non
         """
-        if self.hasAcceptableInput() and self.regexValidator.match(self.text()):
+        # La valeur est elle acceptable ?
+        if (self.hasAcceptableInput() and
+                self.regexValidator.match(self.text())):
+            # Oui
+            # Est-elle hors tolérance ?
             if self.isStrange():
+                # Oui -> Orange
                 self.setStyleSheet("color: orange")
             else:
+                # Non -> Vert
                 self.setStyleSheet("color: green;")
             return True
-        if color:
+        # La valeur n'est pas valide
+        # Rouge
+        if color_notvalid:
             self.setStyleSheet("color: red;")
         return False
 
 
 class SaisieHeure(MyLineEdit):
+    """ Champs de saisie heure hhmmss hérité de MyLineEdit
+    """
 
-    def __init__(self, textInit="hhmmss"):
-        """MyLineEdit pour saisir une heure"""
+    def __init__(self, textInit: str = "hhmmss") -> None:
+        """ MyLineEdit pour saisir une heure
+
+        Args:
+            textInit (str, optional): Text initiale. Defaults to "hhmmss".
+        """
         super().__init__(textInit)
         self.setMaxLength(6)
+        # Regex de validation d'heure format hhmmss
         self.regexValidator = heure_re
 
 
 class SaisieAngle(MyLineEdit):
-
-    def __init__(self, textInit="---.----"):
+    """ Champs de saisie d'angle hérité de MyLineEdit
+    """
+    def __init__(self, textInit: str = "---.----") -> None:
         """MyLineEdit pour la saisie d'angle gradiant"""
         super().__init__(textInit)
         self.setMaxLength(len(textInit))
+        # Regex de validation d'angle
         self.regexValidator = angle_re
+        # Action en cas d'édition
         self.selectionChanged.connect(self.changeSelection)
 
-    def changeSelection(self):
-        """Permet la selection intelligente des données préremplies"""
-        # si tout le text est selectionné
-        if self.selectionLength() == len( self.text() ):
-            # selection uniquement des "--" initialisé
+    def changeSelection(self) -> None:
+        """ Permet la selection intelligente des données préremplies
+        """
+        # Si tout le text est selectionné
+        if self.selectionLength() == len(self.text()):
+            # Selection uniquement des "--" initialisé
             start = self.initext.find("-")
             stop = len(self.text())
             if start > stop:
@@ -185,9 +225,10 @@ class SaisieAngle(MyLineEdit):
                 start, stop
             )
 
-    def rewrite(self):
-        """Permet de reecrire la celule dans le bon format
-        (ex: ajout des 0 manquants pour avoir 4 decimals)"""
+    def rewrite(self) -> None:
+        """ Permet de reecrire la cellule dans le bon format
+            (ex: ajout des 0 manquants pour avoir 4 decimals)
+        """
         try:
             self.setText("%.4f" % ((float(self.text()))), True)
             self.update()
@@ -196,15 +237,19 @@ class SaisieAngle(MyLineEdit):
 
 
 class SaisieMesure(MyLineEdit):
-
-    def __init__(self, textInit="--.-"):
-        """MyLineEdit pour la saisir d'une mesure en nT"""
+    """ Champs de saisie de mesure hérité de MyLineEdit
+    """
+    def __init__(self, textInit: str = "--.-") -> None:
+        """ MyLineEdit pour la saisi d'une mesure en nT
+        """
         super().__init__(textInit)
+        # Regex de validation des mesure
         self.regexValidator = mesure_re
 
-    def rewrite(self):
-        """rewrite() permet de reecrire la celule dans le bon format
-        (ex: ajout des 0 manquants pour avoir 1 decimal)"""
+    def rewrite(self) -> None:
+        """ rewrite() permet de reecrire la celule dans le bon format
+            (ex: ajout des 0 manquants pour avoir 1 decimal)
+        """
         try:
             self.setText("%.1f" % (float(self.text())), True)
             self.update()
@@ -212,23 +257,24 @@ class SaisieMesure(MyLineEdit):
             return
         return
 
-    def isStrange(self):
-        """Override de isStrange() du parent, fournit un detecteur de saisie anormale
+    def isStrange(self) -> bool:
+        """ Override de isStrange() du parent, fournit un detecteur de saisie
+            anormale
 
         Returns:
             bool: True si valeur anormale
         """
-        return float(self.text()) >= 10 or float(self.text()) <= -10
+        return abs(float(self.text())) >= 10
 
 
 class CalibrationAzimuth(QtWidgets.QGroupBox):
 
-    def __init__(self, numVisee, autoValue):
-        """Layout pour la saisie des angle de visé de la cible
+    def __init__(self, numVisee: int, autoValue: dict) -> None:
+        """ Layout pour la saisie des angle de visé de la cible
 
         Args:
             numVisee (int): Numéro de la visée (1 ou 2)
-            autoValue (collection): Valeur d'angle pour l'autocompletion
+            autoValue (dict): Valeur d'angle pour l'autocompletion
         """
         super().__init__()
         # Titre
@@ -249,12 +295,13 @@ class CalibrationAzimuth(QtWidgets.QGroupBox):
         self.setFixedWidth(500)
 
         # Mise en place des triggers
-        self.angleVH.textChanged.connect(lambda: self.updateAngle(self.angleVH.text()))
+        self.angleVH.textChanged.connect(
+            lambda: self.updateAngle(self.angleVH.text()))
         self.angleVH.textEdited.connect(self.stopUpdate)
 
         self.updatable = True  # Autcompletion activé
 
-    def updateAngle(self, angle):
+    def updateAngle(self, angle: str) -> None:
         """autocomplet le deuxieme angle de visée (sonde en bas)
 
         Args:
@@ -266,11 +313,11 @@ class CalibrationAzimuth(QtWidgets.QGroupBox):
         except ValueError:
             pass
 
-    def stopUpdate(self):
+    def stopUpdate(self) -> None:
         """Désactive l'autocomplétion"""
         self.updatable = False
 
-    def getAzi(self):
+    def getAzi(self) -> None:
         """Retourne les données saisis
 
         Returns:
@@ -278,7 +325,7 @@ class CalibrationAzimuth(QtWidgets.QGroupBox):
         """
         return self.angleVH.text(), self.angleVB.text()
 
-    def validate(self):
+    def validate(self) -> None:
         """Valide les angles saisies
 
         Returns:
@@ -289,15 +336,17 @@ class CalibrationAzimuth(QtWidgets.QGroupBox):
 
 class Mesure(QtWidgets.QGroupBox):
 
-    def __init__(self, titre, nomMesure, typeMesure, autoValueAngle, autoValueSec):
+    def __init__(self, titre: str, nomMesure: str, typeMesure: str,
+                 autoValueAngle: dict, autoValueSec: int) -> None:
         """Widget des mesure d'inclinaison et de declinaison
 
         Args:
             titre (str): Nom de la mesure
             nomMesure (str): Nom de la mesure pour transfert des datas
             typeMesure (str): inclinaison ou declinaison
-            autoValueAngle (collection): Valeur d'angle pour l'autocompletion
-            autoValueSec (int): Temps estimé entre deux mesures pour l'autocompletion
+            autoValueAngle (dict): Valeur d'angle pour l'autocompletion
+            autoValueSec (int): Temps estimé entre deux mesures pour
+                                l'autocompletion
         """
         super().__init__()
         # Definition d'un titre
@@ -308,11 +357,10 @@ class Mesure(QtWidgets.QGroupBox):
         self.nomMesure = nomMesure
         # Définition du lapse de temps auto entre les mesures
         self.autoValueSec = autoValueSec
-
         # Définition d'un' layout
         self.layoutMesurePr = QtWidgets.QGridLayout()
-
-        # S'il s'agit d'une mesure d'inclinaison alors permettre la saisie de l'est magnétique
+        # S'il s'agit d'une mesure d'inclinaison alors permettre la saisie de
+        # l'est magnétique
         if self.typeMesure == "inclinaison":
             self.indAngleEst = QtWidgets.QLabel("Est magnétique")
             self.indAngleEst.setAlignment(Qt.AlignCenter)
@@ -325,7 +373,6 @@ class Mesure(QtWidgets.QGroupBox):
             self.indSpace2 = QtWidgets.QLabel("")
             self.layoutMesurePr.addWidget(self.indSpace1, 0, 1)
             self.layoutMesurePr.addWidget(self.indSpace2, 1, 1)
-
         # Definition des indication et des cases de saisie en haut des colonnes
         self.indHeure = QtWidgets.QLabel("HEURE")
         self.indHeure.setAlignment(Qt.AlignCenter)
@@ -336,7 +383,6 @@ class Mesure(QtWidgets.QGroupBox):
         self.layoutMesurePr.addWidget(self.indHeure, 2, 1)
         self.layoutMesurePr.addWidget(self.indAngle, 2, 2)
         self.layoutMesurePr.addWidget(self.indMesure, 2, 3)
-
         # Definition de la liste des 4 ligne de mesures
         self.ligne = []
         # Création des lignes de mesure
@@ -356,29 +402,35 @@ class Mesure(QtWidgets.QGroupBox):
 
             numero = QtWidgets.QLabel(str(i + 1))
             self.ligne.append(
-                {"label": numero, "heure": heure, "angle": angle, "mesure": mesure}
+                {"label": numero,
+                 "heure": heure,
+                 "angle": angle,
+                 "mesure": mesure}
             )
             self.layoutMesurePr.addWidget(self.ligne[i]["label"], 3 + i, 0)
             self.layoutMesurePr.addWidget(self.ligne[i]["heure"], 3 + i, 1)
             self.layoutMesurePr.addWidget(self.ligne[i]["angle"], 3 + i, 2)
             self.layoutMesurePr.addWidget(self.ligne[i]["mesure"], 3 + i, 3)
-
-        self.ligne[0]["heure"].editingFinished.connect(lambda: self.updateHeure(0))
-        self.ligne[1]["heure"].editingFinished.connect(lambda: self.updateHeure(1))
-        self.ligne[2]["heure"].editingFinished.connect(lambda: self.updateHeure(2))
+        # Fonctions pour autocomplete
+        self.ligne[0]["heure"].editingFinished.connect(
+            lambda: self.updateHeure(0))
+        self.ligne[1]["heure"].editingFinished.connect(
+            lambda: self.updateHeure(1))
+        self.ligne[2]["heure"].editingFinished.connect(
+            lambda: self.updateHeure(2))
         # lors de la modification de la valeur de l'angle de la premiere ligne
         self.ligne[0]["angle"].textEdited.connect(
-            lambda: self.updateAngle(float(self.ligne[0]["angle"].text()), False)
+            lambda: self.updateAngle(
+                float(self.ligne[0]["angle"].text()), False)
         )
         # autocompletion permise
         self.stopUpdate(True)
-
         # Mise en place du layout
         self.setFixedWidth(500)
         self.setLayout(self.layoutMesurePr)
 
-    def stopUpdate(self, disable):
-        """Désactive/active l'autocompletion
+    def stopUpdate(self, disable: bool) -> None:
+        """ Désactive/active l'autocompletion
 
         Args:
             disable (bool): True pour desactiver l'autocompletion
@@ -390,8 +442,8 @@ class Mesure(QtWidgets.QGroupBox):
         if self.typeMesure == "inclinaison":
             self.angleEst.setDisabled(disable)
 
-    def updateHeure(self, indexLigne):
-        """Autocomplete pour la datation de la prochaine ligne de mesure
+    def updateHeure(self, indexLigne: int) -> None:
+        """ Autocomplete pour la datation de la prochaine ligne de mesure
 
         Args:
             indexLigne (int): index de la ligne où l'heure a été saisie
@@ -405,12 +457,13 @@ class Mesure(QtWidgets.QGroupBox):
             # Affichage de l'heure de la prochaine mesure
             self.ligne[indexLigne + 1]["heure"].setText(calculedHour)
 
-    def updateAngle(self, angle, includeFirst):
-        """ Autocomplete sur demande des angles
+    def updateAngle(self, angle: str, includeFirst: bool) -> None:
+        """ Autocomplete des angles
 
         Args:
-            angle (str): angle fournit
-            includeFirst (bool): mise à jour de tous les angles ou juste des 3 derniers
+            angle (str): angle fourni
+            includeFirst (bool): mise à jour de tous les angles ou juste des
+                                 3 derniers
         """
         if includeFirst:
             self.ligne[0]["angle"].setText("%.4f" % angle)
@@ -421,9 +474,10 @@ class Mesure(QtWidgets.QGroupBox):
         else:
             self.ligne[1]["angle"].setText("%.4f" % ((angle + 200) % 400))
             self.ligne[2]["angle"].setText("%.4f" % ((400 - angle) % 400))
-            self.ligne[3]["angle"].setText("%.4f" % ((400 - angle - 200) % 400))
+            self.ligne[3]["angle"].setText(
+                "%.4f" % ((400 - angle - 200) % 400))
 
-    def updateEst(self, angle):
+    def updateEst(self, angle: str) -> None:
         """ Autocomplet la saisie de l'est magnetique
 
         Args:
@@ -431,11 +485,11 @@ class Mesure(QtWidgets.QGroupBox):
         """
         self.angleEst.setText("%.4f" % angle)
 
-    def getData(self):
+    def getData(self) -> dict:
         """ Renvoi les mesures sous forme d'un dictionnaire
 
         Returns:
-            dictionnaire: mesure au format:
+            dict: mesure au format:
             {
                 "nom": nom de la mesure
                 "est": angle (ou None),
@@ -461,8 +515,8 @@ class Mesure(QtWidgets.QGroupBox):
         collectionData["nom"] = self.nomMesure
         return collectionData
 
-    def validate(self):
-        """Valide l'ensemble des cases des lignes de la mesure
+    def validate(self) -> bool:
+        """ Valide l'ensemble des cases des lignes de la mesure
 
         Returns:
             bool: Mesures valide ou non
